591c55ef88a2f8a1ffd43a85cbb0166c703ed94e
oleg <oleg20064@gmail.com>
2021-06-05 20:08:13 +0300

    -Теперь можно загружать данные за любой день. Данные на будущее добавлять нельзя. -Пересмотрел систему подсчета км при формировании дока. Бывали случаи, когда 130 + 130 + 142 давали в итоге 401. было это из-за того, что подсчет велся с округлением математическим. Теперь такого не будет и все будет максимально точно. -Исправлена ошибка, из-за которой приложение закрывалось после нажатия на кнопку "отправить на почту". -Исправлен недочет, что файл все равно формировался, даже если не ввели email после нажатия на кнопку "отправить на почту" -Теперь файлы по месяцам формируются разные

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowIcon(QIcon('logistic.ico'))
         database.connect()
         database.create_tables([Data])
+        database.close()
         self.show()
         self.init_ui()
 
@@ -54,11 +55,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButton_2.clicked.connect(self.send_to_email)
 
     def send_to_email(self):
+        email = self.lineEdit_3.text()
+        if not email:
+            self.widget_act('Введите адрес почты получателя!')
+            return
         check, file = self.unload_data()
         if check:
-            email = self.lineEdit_3.text()
-            if not email:
-                self.widget_act('Введите адрес почты получателя!')
             msg = MIMEMultipart()
             msg['From'] = ADDRESS_EMAIL
             msg['To'] = email
@@ -83,15 +85,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             server.quit()
 
     def load_data(self):
+        database.connect()
         if self.lineEdit_2.text() == '':
             self.widget_act('Заполните поле с вашим адресом!')
+            database.close()
             return False
         elif self.lineEdit.text() == '':
             self.widget_act('Заполните поле с адресами!')
+            database.close()
             return False
         else:
-            if Data.get_or_none(date=datetime.datetime.now()):
-                self.widget_act('Сегодня вы уже внесли данные в базу!')
+            if Data.get_or_none(date=self.dateEdit.date().toPyDate()):
+                self.widget_act('За этот день вы уже внесли данные\nв базу!')
+                database.close()
                 return
             else:
                 final_address = self.lineEdit_2.text() + ';' + self.lineEdit.text() + ';' + self.lineEdit_2.text()
@@ -103,10 +109,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     data_list = json.loads(r.text)
                     if data_list['rows'][0]['elements'][0]['status'] == 'NOT_FOUND':
                         self.widget_act('Один из адресов не был найден\nПопробуйте ввести адреса снова!')
+                        database.close()
                         return
                     else:
                         Data(
-                            date=datetime.datetime.now(),
+                            date=self.dateEdit.date().toPyDate(),
                             from_=data_list['origin_addresses'][0],
                             to_=data_list['destination_addresses'][0],
                             distance=data_list['rows'][0]['elements'][0]['distance']['value']
@@ -114,6 +121,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.widget_act('Данные успешно загружены в базу!')
             self.lineEdit.clear()
             self.lineEdit_2.clear()
+            database.close()
 
     def widget_act(self, text_to_send):
         self.widget.label.setText(text_to_send)
@@ -127,23 +135,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
              'Пройдено км.': [item for item in distance]
              }
         )
-        writer = pd.ExcelWriter(f"unload_{datetime.datetime.now().date()}.xlsx",
+        writer = pd.ExcelWriter(f"unload_{datetime.datetime.now().date()}_{self.spinBox.text()}_month.xlsx",
                                 engine='xlsxwriter')
         df.to_excel(writer, sheet_name='Sheet1', index=False)
         writer.save()
         writer.close()
         config = pdfkit.configuration(wkhtmltopdf=f'{os.getcwd()}\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
-        df = pd.read_excel(f"unload_{datetime.datetime.now().date()}.xlsx")
+        df = pd.read_excel(f"unload_{datetime.datetime.now().date()}_{self.spinBox.text()}_month.xlsx")
         df1 = df.replace(np.nan, '', regex=True)
         df1.to_html(f"{os.getcwd()}\\file.html")
-        pdfkit.from_file(f"{os.getcwd()}\\file.html", f'{os.getcwd()}\\unload_{datetime.datetime.now().date()}.pdf',
+        pdfkit.from_file(f"{os.getcwd()}\\file.html",
+                         f'{os.getcwd()}\\unload_{datetime.datetime.now().date()}_{self.spinBox.text()}_month.pdf',
                          configuration=config, options={'encoding': "UTF-8"})
         os.remove(f'{os.getcwd()}\\file.html')
-        os.remove(f'{os.getcwd()}\\unload_{datetime.datetime.now().date()}.xlsx')
+        os.remove(f'{os.getcwd()}\\unload_{datetime.datetime.now().date()}_{self.spinBox.text()}_month.xlsx')
         self.widget_act('Вы успешно выгрузили данные!')
-        return f'{os.getcwd()}\\unload_{datetime.datetime.now().date()}.pdf'
+        return f'{os.getcwd()}\\unload_{datetime.datetime.now().date()}_{self.spinBox.text()}_month.pdf'
 
     def data_processing(self, price_gas):
+        database.connect()
         intermediate_date = ''
         date = []
         from_ = []
@@ -154,15 +164,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         final_distance = 0
         month = self.spinBox.value()
         year = self.spinBox_2.value()
-        data_from_db_with_month = Data.select().where((month == Data.date.month) & (year == Data.date.year))
+        data_from_db_with_month = Data.select().order_by(Data.date).where(
+            (month == Data.date.month) & (year == Data.date.year))
         if not data_from_db_with_month:
             self.widget_act('Данных за этот месяц и год\nне существует!')
+            database.close()
             return
         else:
             for item in data_from_db_with_month:
                 final_distance += item.distance
                 if intermediate_date != str(item.date):
                     distance_list[index] = int(round(distance / 1000, 0))
+                    # distance_list.append('')
                     distance = 0
                 distance += item.distance
                 if str(item.date) not in date:
@@ -175,21 +188,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 from_.append(item.from_)
                 to_.append(item.to_)
             else:
-                distance_list[index] = int(round(distance / 1000, 0))
-                if len(distance_list) < len(date):
+                distance_list[index] = int(round(distance / 1000, 1))
+                while len(distance_list) < len(date):
                     distance_list.append('')
             final_distance = int(round(final_distance / 1000))
             date += ['', '', '']
             from_ += ['', '', '']
             to_ += ['Итого:(км)', 'Итого к выплате:(руб)', 'Цена бензина за 1 л.:']
-            distance_list += [final_distance, round(final_distance / 10 * price_gas, 0), price_gas]
+            distance_list += [final_distance, round(final_distance / 10 * price_gas, 1), price_gas]
+            database.close()
             return self.pandas_processing(date=date, from_=from_, to_=to_, distance=distance_list)
 
     def unload_data(self):
         price_gas = self.doubleSpinBox.value()
         if price_gas == 0.0:
             self.widget_act('Вы не ввели цену бензина!')
-            return False
+            return False, None
         else:
             filename = self.data_processing(price_gas=price_gas)
             if not filename:
@@ -200,9 +214,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 def run():
     try:
-        app = QApplication(sys.argv)
-        window = MainWindow()
-        app.exec_()
+        while True:
+            app = QApplication(sys.argv)
+            window = MainWindow()
+            app.exec_()
     except Exception as exc:
         print(exc)
 
